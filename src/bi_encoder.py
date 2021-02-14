Fix loss computation.

src/bi_encoder.py
@@ -10,18 +10,25 @@ class BiEncoder(nn.Module):
         # Mention embedder
         self.mention_embedder = DistilBertModel.from_pretrained('distilbert-base-uncased')
         self.mention_embedder.train()
+        self.fc_me = nn.Linear(768, 64)
 
         # Entity embedder
         self.entity_embedder = DistilBertModel.from_pretrained('distilbert-base-uncased')
         self.entity_embedder.train()
+        self.fc_ee = nn.Linear(768, 64)
 
     # x represents our data
     def forward(self, mention_inputs, entity_inputs, negative_entity_inputs):
         me = self.mention_embedder(**mention_inputs).last_hidden_state[:, 0]
-        ee = self.entity_embedder(**entity_inputs).last_hidden_state[:, 0]
-        nee = self.entity_embedder(**negative_entity_inputs).last_hidden_state[:, 0]
+        me = self.fc_me(me)
 
-        scores = torch.matmul(me, ee.T)
-        neg_scores = torch.matmul(me, nee.T)
+        ee = self.entity_embedder(**entity_inputs).last_hidden_state[:, 0]
+        ee = self.fc_ee(ee)
+
+        nee = self.entity_embedder(**negative_entity_inputs).last_hidden_state[:, 0]
+        nee = self.fc_ee(nee)
+
+        scores = torch.sum(me * ee, dim=-1)
+        neg_scores = torch.sum(me * nee, dim=-1)
         loss = torch.mean(-scores + neg_scores)
         return me, ee, loss

train_zeshel.py
@@ -20,9 +20,9 @@ def main():
     model = BiEncoder()
     trainset = ZeshelDataset(os.path.join(dir_path, 'zeshel_transformed'), split='train')
     valset = ZeshelDataset(os.path.join(dir_path, 'zeshel_transformed'), split='val')
-    valset = [valset[i] for i in range(10)]
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=2, shuffle=True, num_workers=2)
-    valloader = torch.utils.data.DataLoader(valset, batch_size=2, shuffle=True, num_workers=2)
+    valset = [valset[i] for i in range(30)]
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    valloader = torch.utils.data.DataLoader(valset, batch_size=4, shuffle=True, num_workers=2)
 
     learning_rate = 1e-4
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)