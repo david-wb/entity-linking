b5cc63d8230bf640958b49171009600523b3773f
David Brown <davewbrwn@gmail.com>
2021-03-28 12:21:49 -0600

    Save plots.

--- src/generate_plots.py
+++ src/generate_plots.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,12 +18,12 @@ opacity = 0.8
 rects1 = plt.bar(index, retrieval_rate_bert, bar_width,
                  alpha=opacity,
                  color='b',
-                 label='BERT Base Uncased')
+                 label='BERT base uncased')
 
 rects2 = plt.bar(index + bar_width, retrieval_rate_roberta, bar_width,
                  alpha=opacity,
                  color='g',
-                 label='RoBERTa Base')
+                 label='RoBERTa base')
 
 rects3 = plt.bar(index + 2 * bar_width, retrieval_rate_declutr, bar_width,
                  alpha=opacity,
@@ -30,9 +32,15 @@ rects3 = plt.bar(index + 2 * bar_width, retrieval_rate_declutr, bar_width,
 
 plt.xlabel('k = Number of Candidates')
 plt.ylabel('Retrieval Rate')
-plt.title('Retrieval Rate by Base Encoder')
+plt.title('Retrieval Rates by Base Encoder')
 plt.xticks(index + bar_width, ('k = 1', 'k = 16', 'k = 32', 'k = 64'))
 plt.legend()
-
+plt.ylim(0, 1)
 plt.tight_layout()
-plt.show()
+plot_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static/retrieval_rates_bar_chart.svg')
+if os.path.isfile(plot_file_path):
+   os.remove(plot_file_path)
+plt.savefig(plot_file_path)
+plt.draw()
+plt.waitforbuttonpress(0)
+plt.close()
